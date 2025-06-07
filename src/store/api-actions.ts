2bdb7fae54524d8d778724f432b0f4657cc15b02
cooperfirewalk <cooperfirewalk@gmail.com>
2025-06-07 11:35:21 +0300

    добавляет асинхронные действия логина/логаута

--- src/store/api-actions.ts
+++ src/store/api-actions.ts
@@ -5,6 +5,9 @@ import {Offer} from '../types/offer.js';
 import {loadOffers, setError, requireAuthorization, setOffersDataLoadingStatus} from './action';
 import {APIRoute, AuthorizationStatus, TIMEOUT_SHOW_ERROR} from '../const';
 import {store} from './';
+import {saveToken, dropToken} from '../services/token';
+import {AuthData} from '../types/auth-data';
+import {UserData} from '../types/user-data';
 
 export const clearErrorAction = createAsyncThunk(
   'app/clearError',
@@ -43,5 +46,31 @@ export const checkAuthAction = createAsyncThunk<void, undefined, {
     } catch {
       dispatch(requireAuthorization(AuthorizationStatus.NoAuth));
     }
+  },
+);
+
+export const loginAction = createAsyncThunk<void, AuthData, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'user/login',
+  async ({login: email, password}, {dispatch, extra: api}) => {
+    const {data: {token}} = await api.post<UserData>(APIRoute.Login, {email, password});
+    saveToken(token);
+    dispatch(requireAuthorization(AuthorizationStatus.Auth));
+  },
+);
+
+export const logoutAction = createAsyncThunk<void, undefined, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'user/logout',
+  async (_arg, {dispatch, extra: api}) => {
+    await api.delete(APIRoute.Logout);
+    dropToken();
+    dispatch(requireAuthorization(AuthorizationStatus.NoAuth));
   },
 );
