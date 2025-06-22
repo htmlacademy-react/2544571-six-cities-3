a2d8e52aad9852b0fe9f525dfde404c66ea04e9a
cooperfirewalk <cooperfirewalk@gmail.com>
2025-06-22 14:37:28 +0300

    добавляет ограничение на количество показанных отзывов

--- src/components/reviews-list/reviews-list.tsx
+++ src/components/reviews-list/reviews-list.tsx
@@ -1,15 +1,18 @@
 import ReviewItem from '../review-item/review-item';
 import { Review } from '../../types/review';
 
+const MAX_REVIEWS = 10;
 
 type ReviewListProps = {
   reviews: Review[];
 }
 
-function ReviewsList({reviews}:ReviewListProps): JSX.Element {
+function ReviewsList({ reviews }: ReviewListProps): JSX.Element {
+  const slicedReviews = reviews.slice(0, MAX_REVIEWS);
+
   return (
     <ul className="reviews__list">
-      {reviews.map((review, id) => {
+      {slicedReviews.map((review, id) => {
         const keyValue = `${id}-${review.id}`;
         return (
           <ReviewItem key={keyValue} review={review} />
